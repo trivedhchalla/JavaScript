df8540d8dc41bef846db1dbef55e10a2b93df1d3
trivedhchalla <ctrivedh@gmail.com>
2026-02-18 18:10:39 +0530

    Learning about JS variables

---
+++ Basics/2_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"// ensure modern standard of coding for js 
+
+// alert("Hello")//alert is not defined this is not brower 
+// we are in node js so no alert
+
+// Datatypes in JS
+let name = "Hitesh" // String  
+let age = 10 // number 
+let isLoggedIn = false // boolean 
+let state;
+let dead = null
+
+// ==> Primitive 
+// number => 2^53 
+// bigint 
+// string - ""
+// boolean - false / true
+// stand alone value - null --> empty value and its a object     
+// undefined --> not initialized 
+// symbol --> for checking of uniqueness 
+
+// =>object
+console.log(typeof state)//undefined
+
+console.log(typeof dead)// object
