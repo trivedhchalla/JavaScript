2cdd846da5c5ac705394d95ad41f2a45ab4bb6a4
trivedhchalla <ctrivedh@gmail.com>
2026-02-18 17:42:39 +0530

    learning about variables

---
+++ Basics/1_variables.js
@@ -0,0 +1,22 @@
+const accNum = 141232132
+// declaration of variables 
+let name = "Trivedh"
+var accountEmail = "neuralNetworks@gmail.com"
+accCity = "Jaipur"
+let accountState;
+
+// //changing constants 
+// accNum = 30112006
+// console.log(accNum)//TypeError: Assignment to constant variable.
+
+name = "Hitesh"
+accountEmail = "hc@google.com"
+accCity = "Bengaluru"
+console.table([accNum,name,accountEmail,accCity,accountState])
+
+/*
+let is prefered over var 
+because let is scope based and var is independent of scope 
+
+if we donot initialize a value then its indefined 
+*/
