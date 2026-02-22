626ce69252321434a4078c28f1bef638c728f30f
trivedhchalla <ctrivedh@gmail.com>
2026-02-22 20:12:00 +0530

    Math and Numbers

---
+++ Basics/9_numbersMath.js
@@ -0,0 +1,55 @@
+// const score = 200 
+
+// console.log(score)
+
+// const balance = new Number(100)
+
+// console.log(balance)
+
+// console.log(balance.toString())
+
+// precision after decimal
+// console.log(balance.toFixed(2))
+
+// toPrecision() tells how many digits it should have
+// const bal = 23.35573
+
+// console.log(bal.toPrecision(3))
+
+// console.log(bal.toPrecision(1))
+
+// console.log(bal.toPrecision(4))
+
+// const hundreds = 1000000
+
+// toLocaleString() number system
+// console.log(hundreds.toLocaleString())
+
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// Maths
+
+console.log(Math.PI)
+
+console.log(Math.abs(-2))
+
+console.log(Math.round(2.5))
+
+console.log(Math.ceil(3.4))
+
+console.log(Math.floor(4.9999))
+
+console.log(Math.min(2,3,4))
+
+console.log(Math.max(2,3,4))
+
+console.log(Math.random())// random value btw 0 to 1 
+
+console.log((Math.random() * 10) + 1)
+
+const min = 10
+
+const max = 20
+// for getting a range of number 
+console.log(Math.random() * (max - min + 1) + min)
+
