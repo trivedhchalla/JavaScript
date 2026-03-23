d121d9d05733133b0607e117f6d854e9fe2f7ac1
trivedhchalla <ctrivedh@gmail.com>
2026-03-23 12:49:32 +0530

    Learning about de-construct obj && API intro

---
+++ 02_Basics/05_object3.js
@@ -0,0 +1,18 @@
+// de-constuct a object property
+const course = {
+    name : "JS",
+    price : 999,
+    instructor : "Hitesh"
+}
+
+const {name : n, price : p, instructor :i} = course;
+
+console.log(i)
+console.log(p)
+console.log(n)
+
+// Intro to API-Application Programming Interface
+// API -> a get way to fetch data earlier it used to be XML now its a JSON format or array
+// json -> {"":""}
+// array -> [{},{}]
+// JSON-JavaScript Object Notation
