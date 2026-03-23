09703661f16d243173b316e0d40c854f7a44d2e6
trivedhchalla <ctrivedh@gmail.com>
2026-03-23 11:18:53 +0530

    Yesterdays Notes

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,35 @@
+// singleton means when we create with a constructor a single object is created 
+// Object.create()
+
+// object literals
+const mySym = Symbol("key1");
+const JSUser = {
+    name : "Ustaad",
+    age : 81,
+    location : "Thiruvananthapuram",
+    email : "ustaad@google.com",
+    [mySym] : "my key-1",
+    isLoggedIn : false,
+    "fullName" : "Ustaadoni",
+    lastDaysOfLoggedIn : ["Mon","Thurs","Sat"]
+}
+// --> keys are stored as "keys"
+console.log(JSUser.lastDaysOfLoggedIn);
+console.log(JSUser['email']);
+console.log(JSUser[mySym])
+console.log(JSUser["fullName"])
+
+// updating or changing  
+JSUser.email = "ustaad@yahoo.com";
+// Object.freeze(JSUser);
+JSUser.name = "hi";
+console.log("No change " + JSUser.name)
+console.log(JSUser)
+
+JSUser.greetings = function(){console.log("Hello World!!")};
+
+console.log(JSUser.greetings())
+
+JSUser .greetings = function(){console.log(`Hello Welcome! To ${this.name}`)};
+
+console.log(JSUser.greetings())
