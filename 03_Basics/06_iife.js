6e0969dd39a84a04333f95a5d22124d12fec7ae6
trivedhchalla <ctrivedh@gmail.com>
2026-03-30 01:10:16 +0530

    IIFE->immediately Invoked Fucntion Expresiion

---
+++ 03_Basics/06_iife.js
@@ -0,0 +1,21 @@
+/**
+ * Immediately Invoked Function Expressions
+ * used when we want the function to be executed as we start the website 
+ * used when the functions variables should not be intervened by global scope 
+ * (declaration)() --> iife syntax 
+ * used to remove pollution from gloval scope 
+ * alwayas we ; at end of iife since it does not know the end of scope 
+ * 
+ */
+
+// named iife 
+(function chai() {
+    console.log('DB CONNECTED...')
+})();
+
+// anonumous iife 
+((name) => {
+    console.log(`DB CONNECTED...2 ${name}`)
+})('code aur chai');
+
+
