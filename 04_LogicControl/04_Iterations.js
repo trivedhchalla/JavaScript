3690f95a619b9231c7205535a684a50c263dd9e6
trivedhchalla <ctrivedh@gmail.com>
2026-04-18 22:50:54 +0530

    For loop in JS

---
+++ 04_LogicControl/04_Iterations.js
@@ -0,0 +1,17 @@
+// for 
+for(let indx  = 0; indx < 10 ; indx ++){
+    if(indx == 5) continue;
+    console.log(indx);
+}
+for(let indx  = 0; indx < 10 ; indx ++){
+    console.log(`Outter Loop ${indx} `);
+    for(let i = 0; i <= 10; i++){
+        console.log(`Outter Loop ${indx} Inner Loop ${i}`);
+    }
+}
+
+// break and continue 
+/**
+ * break ---> the execution comes out for the loop
+ * continue ---> skips the part after the and goes from i to i + 1
+ */
